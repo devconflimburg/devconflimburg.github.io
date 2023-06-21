f3fd6f203e76f41289c584bfbd9d4910da5037b6
bohanssen <bohanssen@gmail.com>
2023-06-21 23:29:11 +0200

    backup teruggezet

--- js/configuration-staging.js
+++
@@ -1,5 +0,0 @@
-localStorage["aws-congnito-user-pool-id"] = "eu-north-1_r2049HoTq";
-localStorage["aws-congnito-app-id"] = "b27k62mb5vg2plihjjof2a27yq";
-const api_url = "https://t7xm7ozeerbffmmjaououimma4.appsync-api.eu-north-1.amazonaws.com/graphql";
-const api_ws = "wss://t7xm7ozeerbffmmjaououimma4.appsync-realtime-api.eu-north-1.amazonaws.com/graphql";
-const api_key = "da2-dp2rzethznd3bah327fvspx35y";
