92cba04736292bbf4e1e2089d76ac79e98739c4f
draftsman-io <system@draftsman.io>
2023-06-28 22:08:26 +0000

    D1562559209:devconf:ManagementSystem:build:2023-06-28T21.51.17

--- js/configuration-staging.js
+++ js/configuration-staging.js
@@ -1,5 +1,5 @@
-localStorage["aws-congnito-user-pool-id"] = "eu-north-1_r2049HoTq";
-localStorage["aws-congnito-app-id"] = "b27k62mb5vg2plihjjof2a27yq";
-const api_url = "https://t7xm7ozeerbffmmjaououimma4.appsync-api.eu-north-1.amazonaws.com/graphql";
-const api_ws = "wss://t7xm7ozeerbffmmjaououimma4.appsync-realtime-api.eu-north-1.amazonaws.com/graphql";
-const api_key = "da2-dp2rzethznd3bah327fvspx35y";
+localStorage["aws-congnito-user-pool-id"] = "eu-north-1_TAwvR3HIn";
+localStorage["aws-congnito-app-id"] = "qrmdj6erjzd5lduhxkf2ubp2ji";
+const api_url = "https://5vvszgerine33ewcjoxphrz66u.appsync-api.eu-north-1.amazonaws.com/graphql";
+const api_ws = "wss://5vvszgerine33ewcjoxphrz66u.appsync-realtime-api.eu-north-1.amazonaws.com/graphql";
+const api_key = "da2-njwu5jlnizflxdo7n37qq2goou";
--- js/configuration.js
+++ js/configuration.js
@@ -1,5 +1,5 @@
 localStorage["aws-congnito-user-pool-id"] = "eu-north-1_qcaaMq9yf";
-localStorage["aws-congnito-app-id"] = "bv4epfgssrfwnnmynftizyz3he";
-const api_url = "https://oj4m644hbne75o3a45xjxnywou.appsync-api.eu-north-1.amazonaws.com/graphql";
-const api_ws = "wss://oj4m644hbne75o3a45xjxnywou.appsync-realtime-api.eu-north-1.amazonaws.com/graphql";
-const api_key = "da2-mluyvcms6zbg7kqn4xt6wllyte";
+localStorage["aws-congnito-app-id"] = "gck72ftbfbeorm74rxrrwc3no4";
+const api_url = "https://4s743zy6qzhy3lnoawcck34e7a.appsync-api.eu-north-1.amazonaws.com/graphql";
+const api_ws = "wss://4s743zy6qzhy3lnoawcck34e7a.appsync-realtime-api.eu-north-1.amazonaws.com/graphql";
+const api_key = "da2-qdiap32b5bdzxecsjwxa7x2lzi";
