c0b22e12bc99c73930c5de33a4b261bedaf1643f
bohanssen <bohanssen@gmail.com>
2023-07-15 01:11:41 +0200

    Website naar staging omgeving verwezen

--- js/configuration.js
+++ js/configuration.js
@@ -1,5 +1,5 @@
-localStorage["aws-congnito-user-pool-id"] = "eu-north-1_qcaaMq9yf";
-localStorage["aws-congnito-app-id"] = "6451hm52o4h5tguvjucm7qccr9";
-const api_url = "https://4s743zy6qzhy3lnoawcck34e7a.appsync-api.eu-north-1.amazonaws.com/graphql";
-const api_ws = "wss://4s743zy6qzhy3lnoawcck34e7a.appsync-realtime-api.eu-north-1.amazonaws.com/graphql";
-const api_key = "da2-qdiap32b5bdzxecsjwxa7x2lzi";
+localStorage["aws-congnito-user-pool-id"] = "eu-north-1_TAwvR3HIn";
+localStorage["aws-congnito-app-id"] = "qrmdj6erjzd5lduhxkf2ubp2ji";
+const api_url = "https://5vvszgerine33ewcjoxphrz66u.appsync-api.eu-north-1.amazonaws.com/graphql";
+const api_ws = "wss://5vvszgerine33ewcjoxphrz66u.appsync-realtime-api.eu-north-1.amazonaws.com/graphql";
+const api_key = "da2-njwu5jlnizflxdo7n37qq2goou";
