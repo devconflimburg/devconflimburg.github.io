82eb83c4782c14412d74e172a1fc86adf2cfeb72
bohanssen <bohanssen@gmail.com>
2023-07-15 00:20:19 +0200

    -

--- js/inschrijven.js
+++ js/inschrijven.js
@@ -1,18 +1,33 @@
+//closeDate "20-03-2024"
+//openDate"31-01-2024"
 
-function schrijf_in(form){
-    console.log(form);
-    mutation_query = `mutation RegisterVisitor($input: RegisterVisitorCommand = {username: "",screenName: ""}) {
-    		User {
-    			register(input: $input){
-    				correlationId
-    			}
-    		}
-    	}`;
-    Draftsman.mutation(mutation_query,(data,errors) => {
-        console.log(data);
-        console.log(errors);
-    },variables={"input" : {
-        "username" : form.email,
-        "screenName" : form.voornaam + " " + form.achternaam
-    }},anonymous=true);
+function inschrijvingGeopend(registrationManager){
+    try{
+        var openDate = moment(registrationManager.openDate, 'DD-MM-YYYY');
+        var closeDate = moment(registrationManager.closeDate, 'DD-MM-YYYY');
+        var check = new Date();
+        return check >= openDate && check <= closeDate && registrationManager.registeredVisitors < registrationManager.maxVisitors;
+    } catch {
+        return false;
+    }
+}
+
+function closedReason(registrationManager){
+    try{
+        var openDate = moment(registrationManager.openDate, 'DD-MM-YYYY');
+        var closeDate = moment(registrationManager.closeDate, 'DD-MM-YYYY');
+        var check = new Date();
+        if (check < openDate){
+            return "Inschrijving opent " + openDate.from(check);
+        } else if (check > closeDate){
+            return "Inschrijving gesloten"
+        } else if (registrationManager.registeredVisitors >= registrationManager.maxVisitors){
+            return "Geen seats beschikbaar";
+        }
+    } catch {
+        return "";
+    }
+}
+function getCloseText(registrationManager){
+    return moment(registrationManager.closeDate, 'DD-MM-YYYY').add(1,'days').fromNow()
 }
---
+++ js/programma.js
@@ -0,0 +1,55 @@
+
+function toMinutes(time_str) {
+    // Extract hours, minutes and seconds
+    var parts = time_str.split(':');
+    // compute  and return total seconds
+    return parts[0] * 60 + parts[1]*1;
+}
+
+var lines = [];
+function get_grid_lines(programma){
+    try {
+        var difference = Math.abs(toMinutes(programma["get"]["end"]) - toMinutes(programma["get"]["start"]));
+        var target = new Date("1970-01-01T" + programma["get"]["start"]);
+        console.log(target);
+        lines = [];
+        [...Array(Math.floor(difference/15)).keys()].forEach(x => {
+            let time = target.toTimeString().split(":");
+            lines.push(`${time[0]}:${time[1]}`);
+            target = new Date(target.getTime() + 15*60000);
+        });
+    } catch {}
+    return lines;
+}
+
+var rooms = ["Forum",
+             "Social zone",
+             "No-code-zone",
+             "Scriptingroom",
+             "Hackingdorm"];
+var talks = rooms.map(x => []);
+var classes = ["one","two","three","four","five"]
+function get_talks(programma){
+    try {
+        var prev = rooms.map(x => programma.get.start);
+        console.log(programma.get.programItem);
+        programma.get.programItem.forEach(x => {
+            let index = rooms.indexOf(x.room);
+            let margin = Math.abs(toMinutes(x.startTime) - toMinutes(prev[index]));
+            x.margin = Math.round(margin/15 * 100);
+            x.class = classes[index];
+            talks[index].push(x);
+            prev[index] = x.endTime;
+        });
+    } catch {}
+    talks = talks.filter(x => x.length != 0);
+    return talks;
+}
+
+function get_rooms(programma){
+    let used_rooms = [];
+    programma.get.programItem.forEach(x => {
+        used_rooms.push(x.room);
+    });
+    return rooms.filter(x => used_rooms.indexOf(x) != -1);
+}
