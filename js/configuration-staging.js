d01925d1fa6c247c98d6f8f7fa3e021720e3c9b0
draftsman-io <system@draftsman.io>
2024-01-21 21:55:55 +0000

    D1562559209:devconf:ManagementSystem

--- js/configuration-staging.js
+++ js/configuration-staging.js
@@ -1,8 +1,6 @@
-if (window.location.search.includes("staging")){
-    console.log("Connected to staging");
-    localStorage["aws-congnito-user-pool-id"] = "eu-central-1_DkbPGBi2e";
-    localStorage["aws-congnito-app-id"] = "bmn1tkt5uoc6jopumotfcs310";
-    api_url = "https://446q7mzyg5eclktd2fsdh3vbhq.appsync-api.eu-central-1.amazonaws.com/graphql";
-    api_ws = "wss://446q7mzyg5eclktd2fsdh3vbhq.appsync-realtime-api.eu-central-1.amazonaws.com/graphql";
-    api_key = "da2-tngt7pwhwjbgtbpticb4275qtu";
+if (localStorage["staging-environment"]){	localStorage["aws-congnito-user-pool-id"] = "eu-central-1_DkbPGBi2e";
+	localStorage["aws-congnito-app-id"] = "bmn1tkt5uoc6jopumotfcs310";
+	const api_url = "https://446q7mzyg5eclktd2fsdh3vbhq.appsync-api.eu-central-1.amazonaws.com/graphql";
+	const api_ws = "wss://446q7mzyg5eclktd2fsdh3vbhq.appsync-realtime-api.eu-central-1.amazonaws.com/graphql";
+	const api_key = "da2-tngt7pwhwjbgtbpticb4275qtu";
 }
