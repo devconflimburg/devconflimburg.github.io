cd9b282cc247e7bdb0dbf47cd5eeb50221e37f15
draftsman-io <system@draftsman.io>
2024-01-23 18:04:14 +0000

    D1562559209:devconf:ManagementSystem

--- js/configuration.js
+++ js/configuration.js
@@ -3,8 +3,8 @@ var api_ws = "";
 var api_key = "";
 
 if (!localStorage["staging-environment"] || localStorage["staging-environment"] == "false"){
-    console.log("Connected to production");
-    localStorage["aws-congnito-user-pool-id"] = "eu-central-1_HNt1QehSI";
+	console.log("Connected to production")
+	localStorage["aws-congnito-user-pool-id"] = "eu-central-1_HNt1QehSI";
 	localStorage["aws-congnito-app-id"] = "464mjnisild08l6cvvga00cho8";
 	api_url = "https://xvy5vktksjd37mgdfaoyo7nlaq.appsync-api.eu-central-1.amazonaws.com/graphql";
 	api_ws = "wss://xvy5vktksjd37mgdfaoyo7nlaq.appsync-realtime-api.eu-central-1.amazonaws.com/graphql";
