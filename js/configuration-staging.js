546c51a64d98b9c8bfd36a95c6238eeaa45b5a85
draftsman-io <system@draftsman.io>
2025-01-13 20:38:51 +0000

    D1562559209:devconf:ManagementSystem

--- js/configuration-staging.js
+++ js/configuration-staging.js
@@ -1,10 +1,10 @@
 if (localStorage["staging-environment"] && localStorage["staging-environment"] == "true"){
 	console.log("Connected to staging");
 	stage = "staging";
-	bucket = "d1ltfpqfbu3vb6.cloudfront.net";
+	bucket = "dflrif08z6f0f.cloudfront.net";
 	localStorage["aws-congnito-user-pool-id"] = "eu-central-1_DBjWIiyoC";
-	localStorage["aws-congnito-app-id"] = "1ukgkef1vih5vpersncirt2kt5";
-	localStorage["aws-congnito-ui"] = "https://dms-staging-oavixm.auth.eu-central-1.amazoncognito.com";	api_url = "https://auowflerkjgvldafa36nj35e3y.appsync-api.eu-central-1.amazonaws.com/graphql";
-	api_ws = "wss://auowflerkjgvldafa36nj35e3y.appsync-realtime-api.eu-central-1.amazonaws.com/graphql";
-	api_key = "da2-s6liefunibfn5bfzmny6bdafhm";
+	localStorage["aws-congnito-app-id"] = "3knvvadsdc3lh8bldm0di2ub29";
+	localStorage["aws-congnito-ui"] = "https://dms-staging-oavixm.auth.eu-central-1.amazoncognito.com";	api_url = "https://xrvxjlqkwzhxbbf3d4q3v6yi3a.appsync-api.eu-central-1.amazonaws.com/graphql";
+	api_ws = "wss://xrvxjlqkwzhxbbf3d4q3v6yi3a.appsync-realtime-api.eu-central-1.amazonaws.com/graphql";
+	api_key = "da2-aj7zshshxbcvvnagqepkezmr7q";
 }
