d39d470db5a64f65e5db06890a320743446ab89b
draftsman-io <system@draftsman.io>
2025-01-15 09:58:45 +0000

    D1562559209:devconf:ManagementSystem

--- js/configuration-staging.js
+++ js/configuration-staging.js
@@ -4,8 +4,7 @@ if (localStorage["staging-environment"] && localStorage["staging-environment"] =
 	bucket = "dflrif08z6f0f.cloudfront.net";
 	localStorage["aws-congnito-user-pool-id"] = "eu-central-1_DBjWIiyoC";
 	localStorage["aws-congnito-app-id"] = "3knvvadsdc3lh8bldm0di2ub29";
-	localStorage["aws-congnito-ui"] = "https://dms-staging-oavixm.auth.eu-central-1.amazoncognito.com";
-	api_url = "https://xrvxjlqkwzhxbbf3d4q3v6yi3a.appsync-api.eu-central-1.amazonaws.com/graphql";
+	localStorage["aws-congnito-ui"] = "https://dms-staging-oavixm.auth.eu-central-1.amazoncognito.com";	api_url = "https://xrvxjlqkwzhxbbf3d4q3v6yi3a.appsync-api.eu-central-1.amazonaws.com/graphql";
 	api_ws = "wss://xrvxjlqkwzhxbbf3d4q3v6yi3a.appsync-realtime-api.eu-central-1.amazonaws.com/graphql";
 	api_key = "da2-aj7zshshxbcvvnagqepkezmr7q";
 }
